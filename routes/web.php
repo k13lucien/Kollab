928c03bacbc11dd01fc026d480a5aa3f28919bfc
ismailaCONGOMBO <sommailacongombo@gmail.com>
2025-11-04 13:30:56 +0000

    debugs

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,5 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json(['message' => 'Kollab API is running', 'status' => 'success']);
 });
